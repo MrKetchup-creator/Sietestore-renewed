d42ac31508877bb59a432c6ba062d4c41fddf6cc
Jeison <jc3403591@gmail.com>
2026-05-10 15:15:50 -0500

    feat: implementar CRUD de productos y persistencia JPA (DAWPSS-6)

---
+++ src/test/java/com/SieteStore/SietestoreInit/SietestoreInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SieteStore.SietestoreInit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SietestoreInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
