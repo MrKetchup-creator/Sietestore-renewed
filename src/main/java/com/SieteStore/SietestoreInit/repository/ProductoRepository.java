edab106951d55c7998989dac93d318e495ecadaf
Jeison <jc3403591@gmail.com>
2026-06-02 07:12:35 -0500

    Feat: Implementado endpoint dinámico de filtrado por categoría y talla respetando soft delete (DAWPSS-10)

--- src/main/java/com/SieteStore/SietestoreInit/repository/ProductoRepository.java
+++ src/main/java/com/SieteStore/SietestoreInit/repository/ProductoRepository.java
@@ -4,7 +4,20 @@ import com.SieteStore.SietestoreInit.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    List<Producto> findByActivoTrue(); // Para no mostrar productos "eliminados"
+// Cambiamos el segundo parámetro de Long a Integer
+public interface ProductoRepository extends JpaRepository<Producto, Integer> { 
+
+    List<Producto> findByActivoTrue();
+
+    @Query("SELECT p FROM Producto p WHERE " +
+           "(:idCategoria IS NULL OR p.idCategoria = :idCategoria) AND " +
+           "(:talla IS NULL OR p.talla = :talla) AND " +
+           "p.activo = true")
+    List<Producto> filtrarProductos(
+        @Param("idCategoria") Integer idCategoria, // Cambiado de Long a Integer
+        @Param("talla") String talla
+    );
 }
