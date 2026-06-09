a56e6e8cefb030c98f40204b2ba9bbcd741f5720
Jeison <jc3403591@gmail.com>
2026-06-09 13:00:49 -0500

    Firebase and everything else connected

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // assets/js/config.js
 const API_BASE_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
-    ? 'http://localhost:8080' // Para pruebas locales
-    : 'https://tu-backend-en-render.onrender.com'; // Para producción
+    ? 'http://localhost:8080'
+    : 'https://sietestore-backend.onrender.com';
 
 export { API_BASE_URL };
